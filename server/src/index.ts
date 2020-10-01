009c176532dc36e414fb147d95ae374e36fe89c3
Robi Hidayat <robihdy@gmail.com>
2020-10-01 10:31:12 +0700

    setup cors and fix register problem

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,7 @@ import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import { MyContext } from './types';
+import cors from 'cors';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -22,6 +23,12 @@ const main = async () => {
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
 
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
   app.use(
     session({
       name: 'iask',
@@ -49,7 +56,7 @@ const main = async () => {
     context: ({ req, res }): MyContext => <MyContext>{ em: orm.em, req, res },
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, cors: false });
 
   app.listen(5000, () => {
     console.log('listening on port 5000');
